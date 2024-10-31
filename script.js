477af44051378cf3d82522b7adf72b355e770a16
Wellington0liveira <142173415+Wellington0liveira@users.noreply.github.com>
2024-10-31 09:37:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ document.getElementById("nao").addEventListener("click", () => {
   const container = document.querySelector(".container");
   container.innerHTML = `
     <h1>Você clicou em não</h1>
-    <button id="novo-sim" class="btn">Sim</button>
+    <button id="novo-sim">Sim</button>
   `;
 
-  // Adiciona um evento de clique para o novo botão "Sim"
   document.getElementById("novo-sim").addEventListener("click", () => {
     container.innerHTML = "<h1>Você clicou em sim</h1>";
   });
 });
+
+// Função para o botão "Voltar"
+function voltarPagina() {
+  location.reload(); // Recarrega a página, voltando ao estado inicial
+}
