b8946a3e6efd4e7b1d7eb3c8198de69a6f07f1ee
Wellington0liveira <142173415+Wellington0liveira@users.noreply.github.com>
2024-10-31 09:41:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 document.getElementById("sim").addEventListener("click", () => {
   const container = document.querySelector(".container");
   container.innerHTML = "<h1>Você clicou em sim</h1>";
+  mostrarBotaoVoltar(); // Mostra o botão Voltar
 });
 
 document.getElementById("nao").addEventListener("click", () => {
   const container = document.querySelector(".container");
   container.innerHTML = `
     <h1>Você clicou em não</h1>
-    <button id="novo-sim">Sim</button>
+    <button id="novo-sim" class="btn">Sim</button>
   `;
 
   document.getElementById("novo-sim").addEventListener("click", () => {
     container.innerHTML = "<h1>Você clicou em sim</h1>";
+    mostrarBotaoVoltar(); // Mostra o botão Voltar
   });
 });
+
+// Função para mostrar o botão "Voltar"
+function mostrarBotaoVoltar() {
+  document.getElementById("voltar").style.display = "block"; // Mostra o botão Voltar
+}
 
 // Função para o botão "Voltar"
 function voltarPagina() {
