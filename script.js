0d4842f80e8eab16fb769eace88daa18d7fbc487
Wellington0liveira <142173415+Wellington0liveira@users.noreply.github.com>
2024-10-31 09:20:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("sim").addEventListener("click", () => {
+  alert("Você clicou em Sim!");
+});
+
+document.getElementById("nao").addEventListener("click", () => {
+  alert("Você clicou em Não!");
+});
