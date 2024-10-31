c077d161dccdef24a1311a951fa4d9b59d819a3f
Wellington0liveira <142173415+Wellington0liveira@users.noreply.github.com>
2024-10-31 09:26:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("sim").addEventListener("click", () => {
   const container = document.querySelector(".container");
   container.innerHTML = "<h1>Você clicou em sim</h1>";
 });
+
+document.getElementById("nao").addEventListener("click", () => {
+  const container = document.querySelector(".container");
+  container.innerHTML = "<h1>Você clicou em não</h1>";
+});
