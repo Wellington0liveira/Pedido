63acfd7b61a0948b911c2e987a89dfc2e58205e9
Wellington0liveira <142173415+Wellington0liveira@users.noreply.github.com>
2024-10-31 09:27:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ document.getElementById("sim").addEventListener("click", () => {
 
 document.getElementById("nao").addEventListener("click", () => {
   const container = document.querySelector(".container");
-  container.innerHTML = "<h1>Você clicou em não</h1>";
+  container.innerHTML = `
+    <h1>Você clicou em não</h1>
+    <button id="novo-sim">Sim</button>
+  `;
+
+  // Adiciona um evento de clique para o novo botão "Sim"
+  document.getElementById("novo-sim").addEventListener("click", () => {
+    container.innerHTML = "<h1>Você clicou em sim</h1>";
+  });
 });
